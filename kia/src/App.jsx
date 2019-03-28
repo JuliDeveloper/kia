ddf16c7a5a75ead8fe13aee4ff5f4cb0135ca6cb
Julia Romanenko <rja-97@yandex.ru>
2019-03-28 17:52:58 +0300

    added folders

--- kia/src/App.jsx
+++ kia/src/App.jsx
@@ -2,9 +2,10 @@ import React, { Fragment } from 'react';
 import './style.css';
 import './App.css';
 
-
 function App() {
-  return <Fragment />
+  return <Fragment>
+  
+  </Fragment>
 }
 
 export default App;
